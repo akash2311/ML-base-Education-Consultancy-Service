4a770205c9b521fae37888a3b1b4d356b2aab611
Akash Pattanayak <akashkumarpattanayak1@gmail.com>
2021-12-08 13:02:38 +0530

    updated Notification pgae

--- src/app/Notification/Notification.js
+++ src/app/Notification/Notification.js
@@ -3,9 +3,127 @@ import React, { Component } from 'react'
 export default class Notification extends Component {
     render() {
         return (
-            <div>
-                This is Notification page.
-            </div>
+            <>
+                <div className="col-md-12 grid-margin">
+                    <div className="card">
+                        <div className="card-body">
+                            <h4 className="card-title text-success">JEE exam to be scheduled on 8 April 2022.</h4>
+                            {/* <p className="card-description"> Use class <code>.text-primary</code>, <code>.text-secondary</code> etc. for text in theme colors </p> */}
+                            {/* <div className="row">
+                  <div className="col-md-6">
+                    <p className="text-primary">.text-primary</p>
+                    <p className="text-success">.text-success</p>
+                    <p className="text-danger">.text-danger</p>
+                    <p className="text-warning">.text-warning</p>
+                    <p className="text-info">.text-info</p>
+                  </div>
+                  <div className="col-md-6">
+                    <p className="text-light bg-dark pl-1">.text-light</p>
+                    <p className="text-secondary">.text-secondary</p>
+                    <p className="text-dark">.text-dark</p>
+                    <p className="text-muted">.text-muted</p>
+                    <p className="text-white bg-dark pl-1">.text-white</p>
+                  </div>
+                </div> */}
+                            <p className="text-secondary">JEE Mains 2022 is to be scheduled on 8th April of 2022. JEE Main 2022 registration will be released on the official website. The JEE Main application form 2022 will be available on jeemain.nta.nic.in. The JEE Main 2022 registration process includes filing of the online registration form, uploading documents, application fee payment and submission. Candidates are advised to check the eligibility details carefully before doing the JEE Main application form 2022.</p>
+                            <p className="text-secondary">The authorities will release the JEE Main admit card 2022 on the official web portal only. Candidates will be able to download their JEE Main 2022 admit card through the official website by entering their application number and date of birth. Candidates should note that JEE Main 2022 admit card will consist of important details includes the candidate's personal details. exam date, time and exam centre</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="col-md-12 grid-margin">
+                    <div className="card">
+                        <div className="card-body">
+                            <h4 className="card-title text-success">UPSC results declared, 761 candidates clear Civil Services Exam 2020; Bihar's Shubham Kumar tops</h4>
+                            {/* <p className="card-description"> Use class <code>.text-primary</code>, <code>.text-secondary</code> etc. for text in theme colors </p> */}
+                            {/* <div className="row">
+                  <div className="col-md-6">
+                    <p className="text-primary">.text-primary</p>
+                    <p className="text-success">.text-success</p>
+                    <p className="text-danger">.text-danger</p>
+                    <p className="text-warning">.text-warning</p>
+                    <p className="text-info">.text-info</p>
+                  </div>
+                  <div className="col-md-6">
+                    <p className="text-light bg-dark pl-1">.text-light</p>
+                    <p className="text-secondary">.text-secondary</p>
+                    <p className="text-dark">.text-dark</p>
+                    <p className="text-muted">.text-muted</p>
+                    <p className="text-white bg-dark pl-1">.text-white</p>
+                  </div>
+                </div> */}
+                            <p className="text-secondary">Civil Services Result 2020 has been released by the Union Public Service Commission on September 24. Shubham Kumar, a native of Bihar who studied at IIT Bombay, topped the exam. “I have been preparing since 2018. I could not believe the results today. I will perform with my fullest capacity in whichever position I'm assigned to,” said Kumar. </p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="col-md-12 grid-margin">
+                    <div className="card">
+                        <div className="card-body">
+                            <h4 className="card-title text-success">NDA Vaccancy 2022 to be announced</h4>
+                            {/* <p className="card-description"> Use class <code>.text-primary</code>, <code>.text-secondary</code> etc. for text in theme colors </p> */}
+                            {/* <div className="row">
+                  <div className="col-md-6">
+                    <p className="text-primary">.text-primary</p>
+                    <p className="text-success">.text-success</p>
+                    <p className="text-danger">.text-danger</p>
+                    <p className="text-warning">.text-warning</p>
+                    <p className="text-info">.text-info</p>
+                  </div>
+                  <div className="col-md-6">
+                    <p className="text-light bg-dark pl-1">.text-light</p>
+                    <p className="text-secondary">.text-secondary</p>
+                    <p className="text-dark">.text-dark</p>
+                    <p className="text-muted">.text-muted</p>
+                    <p className="text-white bg-dark pl-1">.text-white</p>
+                  </div>
+                </div> */}
+                            <p className="text-secondary">NDA vacancies 2022 will be announced along with the notification. NDA (I) 2022 vacancies will be announced on December 22, 2021. NDA II 2022 vacancies will be announced on May 18, 2022. In 2021, a total of 400 vacancies were announced for NDA I and II exams. 
+
+NDA exam is conducted by the Union Public Service Commission (UPSC) for admission to Army, Navy, and Air Force wings of NDA and Indian Naval Academy Courses. The selection process of NDA comprises the written exam and SSB Interview. </p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="col-md-12 grid-margin">
+                    <div className="card">
+                        <div className="card-body">
+                            <h4 className="card-title text-success">Top 5 Government Engineering College in Odisha.</h4>
+                            {/* <p className="card-description"> Use class <code>.text-primary</code>, <code>.text-secondary</code> etc. for text in theme colors </p> */}
+                            {/* <div className="row">
+                  <div className="col-md-6">
+                    <p className="text-primary">.text-primary</p>
+                    <p className="text-success">.text-success</p>
+                    <p className="text-danger">.text-danger</p>
+                    <p className="text-warning">.text-warning</p>
+                    <p className="text-info">.text-info</p>
+                  </div>
+                  <div className="col-md-6">
+                    <p className="text-light bg-dark pl-1">.text-light</p>
+                    <p className="text-secondary">.text-secondary</p>
+                    <p className="text-dark">.text-dark</p>
+                    <p className="text-muted">.text-muted</p>
+                    <p className="text-white bg-dark pl-1">.text-white</p>
+                  </div>
+                </div> */}
+                            <p className="text-secondary">Orissa is home to prestigious institutes to pursue a BTech course like National Institute of Technology (NIT), Rourkela; Indian Institute of Technology (IIT), Bhubaneswar; Veer Surendra Sai University of Technology (VSSUT), Orissa; Siksha 'O' Anusandhan University (SOA), Bhubaneswar; C.V. Raman College of Engineering, Bhubaneswar; Silicon Institute of Technology, Bhubaneswar; International Institute of Information Technology (IIIT), Bhubaneswar; Gandhi Engineering College, Bhubaneswar, etc. List of Top 10 Engineering COlleges are listed below.</p>
+                            <p className="text-info">1. National Institute of Technology(NIT), Rourkela</p>
+                            <p className="text-secondary">National Institute of Technology Rourkela is undoubtedly the best engineering college in Odisha. QS University Rankings: BRICS 2016 has figured NIT Rourkela in it's list of ranking of the 250 top universities in the five BRICS countries (Brazil, Russia, India, China and South Africa). Simillarly Times World Ranking of Universities has figured NIT Rourkela in it's list of top 980 universities in the world. The honorable President of India, Shri Pranab Mukherjee has visited the Institute. Among others Padma Shri Professor Deepak B. Phatak, Salim Merchant and Sulaiman Merchant Musician Brothers, Sonu Nigam, Padmashri Geeta Chandran, Pandit Vishwa Mohan Bhatt and many eminent personalities has visited the institute. </p>
+                            <p className="text-info">2. Indian Institute of Technology(IIT), Bhubaneswar</p>
+                            <p className="text-secondary">IIT Bhubaneswar is indeed the top engineering college in Odisha. Indian Institute of Technology Bhubaneswar is a public engineering institution established by the government of India in 2008, located at Bhubaneswar, Odisha, India. The institute admits students into B. Tech, M. Tech and Ph.D. programs. IIT Bhubaneswar became the first amongst the eight new IITs to become operational with a sanctioned intake of 40 students each in the disciplines of Civil, Mechanical and Electrical Engineering. Classes for the first batch of students started from the campus of IIT Kharagpur at Kharagpur. The Government of Odisha has allotted about 936 acres of Government land for permanent campus of the institute. </p>
+                            <p className="text-info">3. Veer Surendra Sai University of Technology(VSSUT), Burla</p>
+                            <p className="text-secondary">VSSUT Burla will rank top in the list of govt engineering colleges. The Veer Surendra Sai University of Technology (VSSUT) Odisha was formed by Orissa Act 9 of 2009 by converting University College of Engineering (UCE), Burla to a non-affiliating Unitary University and came into force by issue of notification by the Industries Department, Government of Odisha from 1st day of July 2009. This legendary University is named after Veer Surendra Sai, the great Indian freedom fighter who was born on 23rd January 1809 and sacrificed his life fighting against the British and died in obscurity in Asirgarh Jail on 28 February 1884. The University occupies nearly 300 acres of prime land in Burla. The efforts and expertise of VSSUT graduates have been major contributors in the planning and construction of Odisha's infrastructure. VSSUT provides its students with modern educational facilities while retaining traditional values, as well as using its strong industrial contacts to mold young, talented individuals who can compete in the global arena.  </p>
+                            <p className="text-info">4. College ofEngineering and Technology(CET), Bhubaneswar</p>
+                            <p className="text-secondary">CET Bhubaneswar is the 4th best govt engineering college in Bhubaneswar in Odisha. The College of Engineering and Technology, Bhubaneswar was established by the Government of Odisha in 1981 to meet the growing technical man power need in the State. It was a Constituent College of the Odisha University of Agriculture and Technology, Bhubaneswar since inception. After creation of a Technical University for Odisha State, the College has become a Constituent College of Biju Patnaik University of Technology (BPUT), Odisha with effect from 09th July, 2002 as per section-37(1) of BPUT Act, 2002.  </p>
+                            <p className="text-info">5. Indira Gandhi Institute of Technology (IGIT), Sarang </p>
+                            <p className="text-secondary">Indira Gandhi Institute of Technology (IGIT), Sarang was established in the year of 1982 and was managed directly by the Govt. of Odisha in the name of Odisha College of Engineering (OCE). Prior to this, since 1981, the institute in the name of Modern Polytechnic (MPT) was offering Diploma Courses in Civil, Electrical, Mechanical, Mining Survey Engineering.In the year 1987, both OCE and MPT were merged and renamed as IGIT, Sarang and the management was transferred to an Autonomous Society. Within a span of three decades, the institute has grown up to the National level academically.  </p>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
